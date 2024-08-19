1c8f0d6ae2da665929e8243cfaac2169361228c3
Victor Aguilar <victoraguilar90@gmail.com>
2024-08-18 19:20:24 -0700

    support more output images from cli

--- eval/interpolator_cli.py
+++ eval/interpolator_cli.py
@@ -145,7 +145,7 @@ def _output_frames(frames: List[np.ndarray], frames_dir: str):
     tf.io.gfile.makedirs(frames_dir)
   for idx, frame in tqdm(
       enumerate(frames), total=len(frames), ncols=100, colour='green'):
-    util.write_image(f'{frames_dir}/frame_{idx:03d}.png', frame)
+    util.write_image(f'{frames_dir}/frame_{idx:05d}.png', frame)
   logging.info('Output frames saved in %s.', frames_dir)
 
 
